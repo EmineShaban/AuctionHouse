15d00008fd45a522772e62452caaac999fabed3b
Emine Shaban <emineshaban@icloud.com>
2022-10-29 10:32:03 +0300

    create User model

--- models/User.js
+++ models/User.js
@@ -2,27 +2,39 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const { SALT_ROUNDS } = require('../config/env')
 const userSchema = new mongoose.Schema({
+// Email – string (required)
+// Password – string (required)
+// First name – string (required)
+// Last name – string (required)
     email: {
         type: String,
         required: [true, 'Email is required'],
-        unique: true,
+        // unique: true,
     },
-    username: {
-        type: String,
-        required: [true, 'Username is required'],
-        unique: true,
-        match: [/^[a-zA-Z0-9]+$/i, 'Username may contain only english letter and numbers']
-    },
+    // username: {
+    //     type: String,
+    //     required: [true, 'Username is required'],
+    //     unique: true,
+    //     match: [/^[a-zA-Z0-9]+$/i, 'Username may contain only english letter and numbers']
+    // },
     password: {
         type: String,
         required: true,
     },
-    bookedHotels: {
+      firstName: {
         type: String,
+        required: true,
     },
-    offeredHotels: {
+    lastName: {
         type: String,
-    }
+        required: true,
+    },
+    // bookedHotels: {
+    //     type: String,
+    // },
+    // offeredHotels: {
+    //     type: String,
+    // }
 })
 
 userSchema.pre('save', function (next) {
